b1e7f48456fe6c53d53d50123d45680580db6550
nikox94 <nikola.yuroukov@gmail.com>
2015-11-01 16:06:08 +0000

    Finished homework.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -23,25 +23,69 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+  return false if arr.size == 1
+  # Actually we have no limitation on
+  # positiveness of numbers so we should
+  # consider all cases.
+  # Hence a sort might not be the most appropriate thing to do yet.
+  for i in 0..(arr.size-1)
+    for j in (i+1)..arr.size
+      return true if i+j == n
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  c = s[0]
+  return !(/[[:alpha:]&&[^AEIOUaeiou]]/ =~ c).nil?
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  return true if s == "0" || s == "00"
+  return false if s.length < 3
+  if /(^[01]*$)/ =~ s
+    s_rev = s.reverse
+    if s_rev[0]=='0' && s_rev[1]=='0'
+      return true
+    end
+  end
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_reader :isbn
+  attr_reader :price
+  def initialize(isbn, price)
+    self.isbn=(isbn)
+    self.price=(price)
+  end
+  def isbn=(new_isbn)
+    raise ArgumentError if !new_isbn.is_a? String
+    raise ArgumentError if new_isbn.empty?
+    @isbn=new_isbn
+  end
+  def price=(new_price)
+    raise ArgumentError if !new_price.is_a? Numeric
+    raise ArgumentError if new_price <= 0
+    @price = new_price
+  end
+  def price_as_string
+    require 'bigdecimal'
+    intsum = (@price*100).round
+    rev = intsum.to_s.reverse
+    result = rev[0,2] + '.' + rev[2..rev.size] + '$'
+    result.reverse
+  end
 end
